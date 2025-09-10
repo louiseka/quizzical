7a8e08404cf9d6a3711c2ff864251ea565361c5d
Louise Aldridge <louisealdridge88@gmail.com>
2025-09-10 16:32:05 +0100

    Test added for Home component

---
+++ src/components/Home.test.jsx
@@ -0,0 +1,32 @@
+import { test, expect, vi, describe } from "vitest";
+import { userEvent } from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+
+import Home from "./Home"
+
+describe("Quiz Component", () => {
+    test("renderStartQuiz prop is called upon clicking Start Quiz", async () => {
+        //Arrange
+        const user = userEvent.setup()
+        const renderStartQuiz = vi.fn()
+        render(<Home renderStartQuiz={renderStartQuiz} />)
+        const startBtn = screen.getByRole('button')
+        //Act
+        await user.click(startBtn)
+        //Assert
+        expect(renderStartQuiz).toHaveBeenCalled()
+    })
+
+    test("renders the title", () => {
+        render(<Home />)
+        expect(screen.getByText("Quizzical")).toBeInTheDocument()
+    })
+
+    test("displays the button text", () => {
+        render(<Home />)
+        expect(screen.getByRole('button')).toHaveTextContent("Start Quiz")
+    })
+
+})
+
+
