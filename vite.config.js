d0d0ea69011a1801d0632f6fdab7cbb7123df9ee
Louise Aldridge <louisealdridge88@gmail.com>
2025-09-10 15:33:19 +0100

    Installing testing frameworks and library

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/quizzical/',
+  test: {
+    setupFiles: ["./test-setup.js"],
+    environment: 'jsdom'
+  }
 })
