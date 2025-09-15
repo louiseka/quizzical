b7002cbe61a21f1552072373742abe1bbaf979a7
Louise Aldridge <louisealdridge88@gmail.com>
2025-09-15 13:54:38 +0100

    Installing msw

--- test-setup.js
+++ test-setup.js
@@ -1,7 +1,33 @@
 import "@testing-library/jest-dom/vitest";
-import { afterEach } from 'vitest'
+import { afterEach, beforeAll, afterAll } from 'vitest'
 import { cleanup } from "@testing-library/react";
 
+import { setupServer } from 'msw/node';
+import { http } from 'msw';
+
+import mockdata from "./mockdata";
+
+
+// Define handlers using MSW 1.x
+export const restHandlers = [
+    http.get('https://opentdb.com/api.php', (_, res, ctx) => {
+        return res(ctx.json(mockdata));
+    }),
+];
+
+// Set up server
+const server = setupServer(...restHandlers);
+
+// Establish requests interception layer before all tests.
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+// Clean up after all tests are done, preventing this
+// interception layer from affecting irrelevant tests.
+afterAll(() => server.close());
+
+
+// Clean up handlers
 afterEach(() => {
+    server.resetHandlers();
     cleanup();
 });
