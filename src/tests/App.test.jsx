c5643fbbc691c82a5bff8df2138eee628c656955
Louise Aldridge <louisealdridge88@gmail.com>
2026-03-18 11:42:43 +0000

    Updating test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -2,31 +2,27 @@ import { test, describe, vi, expect } from "vitest";
 import { userEvent } from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
-
 import App from "../App";
 
 describe("App", () => {
-    test.todo("The shuffle function shuffles answers")
+  test.todo("The shuffle function shuffles answers");
 
-    test.todo("The start quiz button fetches the quiz")
+  test.todo("The start quiz button fetches the quiz");
 
-    test.todo("Check answers button appears when all answers are selected")
+  test.todo("Check answers button appears when all answers are selected");
 
-    test.todo("Check answer button displays a score out of 5")
+  test.todo("Check answer button displays a score out of 5");
 
-    test("newGame is called upon clicking 'Play Again' btn", async () => {
+  test("clicking 'Play Again' shows the start screen again", async () => {
+    const user = userEvent.setup();
+    render(<App />);
 
-        //Arrange
-        const user = userEvent.setup()
-        const newGame = vi.fn()
-        render(<App />)
-        const playAgainBtn = screen.getByRole('button')
+    const playAgainBtn = await screen.findByRole("button", {
+      name: /play again/i,
+    });
 
-        //Act 
-        await user.click(playAgainBtn)
+    await user.click(playAgainBtn);
 
-        //Assert
-        expect(newGame).toHaveBeenCalled()
-    })
-})
-
+    expect(screen.getByText(/start quiz/i)).toBeInTheDocument();
+  });
+});
